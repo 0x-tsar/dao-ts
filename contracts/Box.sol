1016d34631b20e442c3f615ea7e2c6d62a7350ca
Gustavo <contato.gustavofernandes@hotmail.com>
2022-03-09 11:26:28 -0300

    basic contract ready

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,2 +1,20 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable{
+
+  uint private value;
+
+  event ValueChanged(uint newValue);
+
+  function store(uint newValue) public onlyOwner {
+    value = newValue;
+    emit ValueChanged(newValue);
+  }
+
+  function retrieve() public view returns(uint) {
+    return value;
+  }
+}
