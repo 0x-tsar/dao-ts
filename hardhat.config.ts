1638c0117251fa881e0fdec32441bddb50205f50
Gustavo <contato.gustavofernandes@hotmail.com>
2022-03-09 11:43:29 -0300

    removing things from hardhat.js

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,9 +19,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 const config: HardhatUserConfig = {
   solidity: "0.8.7",
   networks: {
@@ -29,18 +26,6 @@ const config: HardhatUserConfig = {
       url: process.env.mumbai,
       accounts: [process.env.account!],
     },
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
 
